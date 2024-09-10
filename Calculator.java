76856addc5e403c2c30ad6ec7a2a0b4f5c539498
Arjay <santiano.arjay0124@gmail.com>
2024-09-10 20:43:54 +0800

    Modulus Method

--- Calculator.java
+++ Calculator.java
@@ -8,12 +8,14 @@ public class Calculator{
         int subtract = a - b;
         int multiply = a * b;
         double divide = a / b;
+        double percent = a % b;
 
 
         System.out.println("Addition: " + sum);
         System.out.println("Subtraction: " + subtract);
         System.out.println("Multiplication: " + multiply);
         System.out.println("Division: " + divide);
+        System.out.println("Modulus: " + percent);
 
 
 
