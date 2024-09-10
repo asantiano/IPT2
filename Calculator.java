beea267cbcd48436b5182a6d955d987315e04a4d
Arjay <santiano.arjay0124@gmail.com>
2024-09-10 20:34:57 +0800

    Subtraction Method

--- Calculator.java
+++ Calculator.java
@@ -5,9 +5,11 @@ public class Calculator{
         int b = 7;
 
         int sum = a + b;
+        int subtract = a - b;
 
 
         System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + subtract);
 
 
     }
